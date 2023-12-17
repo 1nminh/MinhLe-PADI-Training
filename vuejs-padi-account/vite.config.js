c519575716d690f3ba199e1cc78b18752297cff3
Lê Ngô Minh <lengominh@gmail.com>
2023-12-17 17:42:19 +0700

    update @ vite alias config

--- vuejs-padi-account/vite.config.js
+++ vuejs-padi-account/vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Make sure this line is present
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
