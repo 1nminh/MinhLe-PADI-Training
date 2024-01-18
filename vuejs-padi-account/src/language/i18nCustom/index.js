bf4f61f2a76307e501a9975542f2ccfe66867c6a
Lê Ngô Minh <lengominh@gmail.com>
2024-01-18 10:43:11 +0700

    Update index.js

--- vuejs-padi-account/src/language/i18nCustom/index.js
+++ vuejs-padi-account/src/language/i18nCustom/index.js
@@ -3,8 +3,8 @@ import translations from "@/language/index.js";
 import router from "@/router/router.js";
 
 const DEFAULT_LANGUAGE = "en-US";
-const storedLanguage =
-  localStorage.getItem("selectedLanguage") || DEFAULT_LANGUAGE;
+// const storedLanguage = localStorage.getItem("lang") || DEFAULT_LANGUAGE;
+// console.log(localStorage.getItem("lang"));
 
 const i18n = createI18n({
   locale: DEFAULT_LANGUAGE,
@@ -12,11 +12,21 @@ const i18n = createI18n({
 });
 
 router.afterEach((to) => {
-  const newLang = router.currentRoute.value.params.lang || DEFAULT_LANGUAGE;
-  if (newLang !== storedLanguage) {
-    i18n.global.locale = newLang;
-    localStorage.setItem("selectedLanguage", newLang);
-  }
+  const newLang = router.currentRoute.value.params.lang;
+  console.log("newLang", newLang);
+  console.log("localStorage.getItem(lang)", localStorage.getItem("lang"));
+  // if (!newLang && localStorage.getItem("lang")) {
+  //   return;
+  // }
+  const storedLanguage = localStorage.getItem("lang") || DEFAULT_LANGUAGE;
+  console.log("newLang", newLang);
+  console.log("storedLanguage", storedLanguage);
+  // if (newLang !== storedLanguage) {
+  //   console.log("here1");
+
+  // }
+  i18n.global.locale = newLang || storedLanguage;
+  localStorage.setItem("lang", i18n.global.locale);
 });
 
 export default i18n;
