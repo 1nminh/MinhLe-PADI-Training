614e6f3b7b82add05663d26a91c7987a25e5b380
Lê Ngô Minh <33169628+1nminh@users.noreply.github.com>
2024-04-18 13:43:58 +0700

    update crawl data

--- vuejs-padi-account/adler.js
+++ vuejs-padi-account/adler.js
@@ -1,40 +1,25 @@
-// function getEnemyInfo(enemyNum) {
-//   let enemyInfo = {
-//     name: "Enemy " + enemyNum,
-//     distance: Math.random() * 100,
-//   };
-//   return enemyInfo;
-// }
-// let count = 0;
-// // Game loop
-// while (count <= 10) {
-//   // Obtain information about the two closest enemies
-//   let enemy1 = getEnemyInfo(11).name;
-//   let dist1 = getEnemyInfo(11).distance;
-//   let enemy2 = getEnemyInfo(22).name;
-//   let dist2 = getEnemyInfo(22).distance;
+// Define the access token, fields, and ad object ID
+const accessToken =
+  "EAAKxmSESpqIBOzqRwpbxdTgx9LKEVDZAb7jktew7lyU9ljB6f4PMf5pX0szQ8P5cD6XExu8Fm0FIq3x8PgrF6k36ZBvvl678UzVdq0ae6DDaEwh2Pf3EwqCSWyRgiZAAgmHIvIU4ryQxRtzPG1kObCy2ctxZBrR99VLwYbrjMpPeZApmPgHaMI62B1ZCWIUgNZ";
+const fields = "impressions";
+const adObjectId = "23852340176180309";
 
-//   // Determine which enemy is closer
-//   let target;
-//   if (dist1 < dist2) {
-//     target = enemy1;
-//   } else {
-//     target = enemy2;
-//   }
-//   count += 1;
-//   // Output the name of the enemy to shoot
-//   console.log(target);
-// }
+// Construct the URL with parameters
+const url = `https://graph.facebook.com/v19.0/${adObjectId}/insights?fields=${fields}&access_token=${accessToken}`;
 
-var a = [];
-
-var b = null;
-
-if (a === b) {
-  console.log("1");
-} else {
-  console.log("2");
-}
-
-// console.log(typeof c);
-// console.log(c);
+// Make a GET request using the Fetch API
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch((error) => {
+    // Handle errors
+    console.error("There was a problem with the request:", error);
+  });
